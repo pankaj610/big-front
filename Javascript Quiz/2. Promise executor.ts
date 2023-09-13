e6378aa14012cb93bb38bfe995c4df9efd93fe76
Pankaj Verma <36084571+pankaj610@users.noreply.github.com>
2023-09-13 09:50:13 +0530

    Create 2. Promise executor.ts

---
+++ Javascript Quiz/2. Promise executor.ts
@@ -0,0 +1,13 @@
+new Promise((resolve, reject) => {
+  resolve(1)
+  resolve(2)
+  reject('error')
+}).then((value) => {
+  console.log(value)
+}, (error) => {
+  console.log('error')
+})
+
+---- Output ----
+
+1
