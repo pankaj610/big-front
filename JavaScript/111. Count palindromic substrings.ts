7f8c362777848755a3a225324bb7c9d9f313fccc
Pankaj Verma <36084571+pankaj610@users.noreply.github.com>
2023-11-01 18:23:59 +0530

    Update 111. Count palindromic substrings.ts

--- JavaScript/111. Count palindromic substrings.ts
+++ JavaScript/111. Count palindromic substrings.ts
@@ -23,3 +23,22 @@ function countPalindromicSubstr(str) {
   }
   return pal;
 }
+
+----- DP Approach ------
+function countPalindromicSubstr(str) {
+  // your code here
+  let len = str.length;
+  let count = 0;
+  let dp = Array(len).fill(false).map(e => Array(len).fill(false));
+  for(let i = 0; i < len; i++) {
+    for(let j = 0; j <= i; j++) { // LOWER MATRIX
+      if(str[i] === str[j]) {  
+          if(i - j <= 1 || dp[i-1][j+1]) {
+            count++;
+            dp[i][j] = true;
+          }
+      }
+    }
+  }
+  return count;
+}
